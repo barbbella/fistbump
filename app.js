29b29afd06e5dcd6d9143c4da4afa802a79e95d9
Kate Jefferson <barbedbella@gmail.com>
2015-06-12 03:14:42 -0700

    troubleshooting extension

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 /**
  * Created by kjefferson on 6/11/2015.
  */
+"use strict";
 
-var app = angular.module('FistBump', ['firebase'])
+var app = angular.module('FistBump', ['firebase', 'ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when("/",{
+                templateUrl: "index.html",
+                controller: "GiveBumpController"
+            })
+            .when("/shoutouts", {
+                templateUrl: "shoutouts.html",
+                controller: "ShoutController"
+            })
+    })
     .controller('GiveBumpController', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
         var auth = new Firebase("https://fistbump.firebaseio.com/");
         auth.authWithOAuthPopup('google', function(error, authData) {
@@ -34,12 +46,24 @@ var app = angular.module('FistBump', ['firebase'])
                 return (current_value || 0) + 1;
             });
             if (shoutout) {
-                var shoutouts = new Firebase(url + "/shoutouts");
-                shoutouts.push(shoutout);
+                var shoutouts = new Firebase("https://fistbump.firebaseio.com/shoutouts");
+                shoutouts.push({name: teammate, shoutout: shoutout});
             }
             $scope.bumpee = '';
             $scope.shoutout = '';
         };
 
+    }])
+    .controller('ShoutController', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
+        var ref = new Firebase("https://fistbump.firebaseio.com/shoutouts");
+        var shoutouts = $firebaseArray(ref);
+        $scope.shouts = [];
+        for (var key in shoutouts) {
+            $scope.shouts.push({
+                name: shoutouts.name,
+                shout: shoutouts.shoutout
+            });
+        }
+        console.log($scope.shouts);
     }]);
 
---
+++ background.js
@@ -0,0 +1,10 @@
+/**
+ * Created by kjefferson on 6/12/2015.
+ */
+
+//example of using a message handler from the inject scripts
+//chrome.extension.onMessage.addListener(
+//    function(request, sender, sendResponse) {
+//        chrome.pageAction.show(sender.tab.id);
+//        sendResponse();
+//    });
